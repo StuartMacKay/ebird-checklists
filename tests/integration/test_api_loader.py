9f91d7028a9757209b92e2b5f3dc24f769863093
Stuart MacKay <smackay@flagstonesoftware.com>
2025-01-07 06:27:26 +0000

    fix: Remove integration test for fetching recent checklists

--- tests/integration/test_api_loader.py
+++ tests/integration/test_api_loader.py
@@ -9,8 +9,3 @@ pytestmark = pytest.mark.django_db
 def test_load_for_date(api_key, country):
     loader = APILoader(api_key)
     loader.load(country, datetime.date.today())
-
-
-def test_load_recent(api_key, country):
-    loader = APILoader(api_key)
-    loader.recent(country, limit=10)
