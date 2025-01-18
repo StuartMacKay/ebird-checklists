b1779d94b81d78f9bf801815dbb8ebfde69dda31
Stuart MacKay <smackay@flagstonesoftware.com>
2025-01-18 09:06:51 +0000

    refactor: Remove methods for fetching observations for partial dates

--- tests/unit/test_observation.py
+++ tests/unit/test_observation.py
@@ -63,46 +63,9 @@ def test_for_location__unsupported_code(location):
         Observation.objects.for_location(location.identifier.lower())
 
 
-def test_for_year__observations_fetched(observation):
-    year = dt.date.today().year
-    observation.checklist.date = observation.checklist.date.replace(year=year)
-    observation.checklist.save()
-    obj = Observation.objects.for_year(year).first()
-    assert obj.id == observation.id
-    assert obj.checklist.date.year == year
-
-
-def test_for_month__observations_fetched(observation):
-    date = dt.date.today()
-    year, month = date.year, date.month
-    observation.checklist.date = observation.checklist.date.replace(
-        year=year, month=month
-    )
-    observation.checklist.save()
-    obj = Observation.objects.for_month(year, month).first()
-    assert obj.id == observation.id
-    assert obj.checklist.date.year == year
-    assert obj.checklist.date.month == month
-
-
-def test_for_day__observations_fetched(observation):
-    date = dt.date.today()
-    year, month, day = date.year, date.month, date.day
-    observation.checklist.date = observation.checklist.date.replace(
-        year=year, month=month, day=day
-    )
-    observation.checklist.save()
-    obj = Observation.objects.for_day(year, month, day).first()
-    assert obj.id == observation.id
-    assert obj.checklist.date.year == year
-    assert obj.checklist.date.month == month
-    assert obj.checklist.date.day == day
-
-
 def test_for_date__observations_fetched(observation):
     date = dt.date.today()
     observation.checklist.date = date
     observation.checklist.save()
     obj = Observation.objects.for_date(date).first()
-    assert obj.id == observation.id
     assert obj.checklist.date == date
