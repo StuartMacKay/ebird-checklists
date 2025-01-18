206589effc89f6009bfea4ebaa923615826fd22e
Stuart MacKay <smackay@flagstonesoftware.com>
2025-01-18 09:30:01 +0000

    refactor: LocationQuerySet.forCountry() only supports eBird codes

---
+++ tests/unit/test_location.py
@@ -0,0 +1,23 @@
+import pytest
+
+from ebird.checklists.models import Location
+from tests.unit.factories import LocationFactory
+
+
+pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture
+def location():
+    return LocationFactory.create()
+
+
+def test_for_country__locations_fetched(location):
+    code = location.country_code
+    obj = Location.objects.for_country(code).first()
+    assert obj.country_code == code
+
+
+def test_for_country__unsupported_code(location):
+    with pytest.raises(ValueError):
+        Location.objects.for_country(location.country_code.lower())
