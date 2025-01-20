0114533a35f3b4e395a4945d88ba5f149d706a03
Stuart MacKay <smackay@flagstonesoftware.com>
2025-01-20 22:41:17 +0000

    New version: 0.4.2 → 0.5.0

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -6,6 +6,9 @@ and this project adheres to `Semantic Versioning <https://semver.org/spec/v2.0.0
 
 Latest
 ------
+
+0.5.0 (2025-01-20)
+------------------
 * Added load_checklist() to APILoader so individual checklists can be updated.
 * Changed APILoader method names; load becomes load_checklists, etc.
 
--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "eBird Checklists"
 copyright = "2024, Stuart MacKay"
 author = "Stuart MacKay"
-release = "0.4.2"
+release = "0.5.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
