8944dc48ed7f1e7791f83963b886df5bd2ddfc88
Stuart MacKay <smackay@flagstonesoftware.com>
2025-02-12 18:29:05 +0000

    refactor: Unify ChecklistQuerySet and ObservationQuerySet methods
    Filtering on location code is easier and more reliable.

--- tests/unit/test_checklist.py
+++ tests/unit/test_checklist.py
@@ -26,10 +26,9 @@ def test_for_country_code__checklists_fetched(location):
     assert obj.location.country_code == code
 
 
-def test_for_country_name__checklists_fetched(location):
-    name = location.country
-    obj = Checklist.objects.for_country(name).first()
-    assert obj.location.country == name
+def test_for_country__unsupported_code(location):
+    with pytest.raises(ValueError):
+        Checklist.objects.for_country(location.country_code.lower())
 
 
 def test_for_state_code__checklists_fetched(location):
@@ -38,10 +37,9 @@ def test_for_state_code__checklists_fetched(location):
     assert obj.location.state_code == code
 
 
-def test_for_state_name__checklists_fetched(location):
-    name = location.state
-    obj = Checklist.objects.for_state(name).first()
-    assert obj.location.state == name
+def test_for_state__unsupported_code(location):
+    with pytest.raises(ValueError):
+        Checklist.objects.for_state(location.state_code.lower())
 
 
 def test_for_county_code__checklists_fetched(location):
@@ -50,10 +48,9 @@ def test_for_county_code__checklists_fetched(location):
     assert obj.location.county_code == code
 
 
-def test_for_county_name__checklists_fetched(location):
-    name = location.county
-    obj = Checklist.objects.for_county(name).first()
-    assert obj.location.county == name
+def test_for_county__unsupported_code(location):
+    with pytest.raises(ValueError):
+        Checklist.objects.for_county(location.county_code.lower())
 
 
 def test_for_location__checklists_fetched(location):
