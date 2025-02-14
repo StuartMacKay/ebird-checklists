2b3890286c38914c5a91fcd6a1350e9fbe6cdde0
Stuart MacKay <smackay@flagstonesoftware.com>
2025-02-14 16:39:53 +0000

    New version: 0.12.0 → 1.0.0

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -7,6 +7,9 @@ and this project adheres to `Semantic Versioning <https://semver.org/spec/v2.0.0
 Latest
 ------
 
+1.0.0 (2025-02-14)
+------------------
+
 0.12.0 (2025-02-14)
 ------------------
 * Add ebird-codes for managing codes used by eBird.
--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "eBird Checklists"
 copyright = "2024, Stuart MacKay"
 author = "Stuart MacKay"
-release = "0.12.0"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
